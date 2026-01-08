89461f7af684d2835f086b6e576ad7eaeb39c1f0
sukasada05 <koramil05sukasada@gmail.com>
2026-01-08 10:12:33 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,45 @@
+// Nama cache
+const CACHE_NAME = 'dashboard-app-v1';
+
+// Assets yang akan di-cache
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activate Service Worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached version atau fetch dari network
+        return response || fetch(event.request);
+      })
+  );
+});
